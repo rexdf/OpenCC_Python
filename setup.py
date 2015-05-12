3e95964b90a4e4030ecda6faf4d3321de61ff627
rexdf <rexdf@vip.qq.com>
2015-05-12 16:58:23 +0800

    fix build script

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, Extension
 
+from sys import platform as _platform
+
+if _platform == "darwin":
+    # OS X
+    link_flags = "-lc++"
+elif _platform == "linux" or _platform == "linux2":
+    link_flags = "-lstdc++"
+
 
 setup(
     name="OpenCC",
@@ -9,6 +17,7 @@ setup(
     ext_modules=[
         Extension("_OpenCC", ["OpenCCPython.i"], ["OpenCC/src"],
                   library_dirs=["OpenCC/build/rel/src"],
-                  libraries=["opencc"]),
+                  libraries=["opencc"],
+                  extra_link_args=[link_flags]),
     ],
 )
